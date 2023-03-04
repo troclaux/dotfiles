dd41e18d71767e179ba769a76562675085a39133
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-03-04 11:23:53 -0300

    adiciona && \ no final das linhas de instalação

--- install_flatpaks.sh
+++ install_flatpaks.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Install Flatpak and the required packages
-sudo apt-get update
-sudo apt-get install flatpak gnome-software-plugin-flatpak
+sudo apt-get update && \
+sudo apt-get install -y flatpak && \
+sudo apt-get install -y flatpak gnome-software-plugin-flatpak && \
 
 # Add the Flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpaks
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub com.mattjakeman.ExtensionManager -y
-flatpak install flathub org.videolan.VLC -y
-flatpak install flathub com.brave.Browser -y
+flatpak install flathub org.telegram.desktop -y && \
+flatpak install flathub com.discordapp.Discord -y && \
+flatpak install flathub org.qbittorrent.qBittorrent -y && \
+flatpak install flathub com.obsproject.Studio -y && \
+flatpak install flathub com.mattjakeman.ExtensionManager -y && \
+flatpak install flathub org.videolan.VLC -y && \
+flatpak install flathub com.brave.Browser -y && \
 flatpak install flathub com.stremio.Stremio -y
