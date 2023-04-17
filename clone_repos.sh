2ec1e43ee62a4af675e228fc6c82a599032044cc
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-17 19:14:33 -0300

    feat: add script that clones github repos

---
+++ clone_repos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/adonovan/gopl.io/ && \
+git clone https://github.com/jwasham/coding-interview-university && \
+git clone https://github.com/MichaelCade/90DaysOfDevOps && \
+git clone https://github.com/bregman-arie/devops-exercises
