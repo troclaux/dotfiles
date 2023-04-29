3072b3e074d2c1375bae039b15920fd999ea9353
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-29 07:30:42 -0300

    feat: add undotree configs

--- troclaux/set.lua
+++ troclaux/set.lua
@@ -10,9 +10,13 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
-vim.o.autoindent = true
 vim.opt.smartindent = true
+-- vim.o.autoindent = true
 
 vim.o.hlsearch = true
 vim.opt.incsearch = true
 
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.signcolumn = "yes"
