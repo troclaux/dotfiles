315810d41170a6b7a46b709a52eba0e4b185dcfc
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-29 09:52:33 -0300

    fix: correct vim indentation

--- troclaux/set.lua
+++ troclaux/set.lua
@@ -10,8 +10,9 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
-vim.opt.smartindent = true
--- vim.o.autoindent = true
+-- vim.opt.smartindent = true
+vim.o.autoindent = true
+vim.cmd('filetype plugin on')
 
 vim.o.hlsearch = true
 vim.opt.incsearch = true
