5ca697c281002e1162cf3fa8c6ef3fd7bc779b1f
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-29 11:21:14 -0300

    fix: solve autoindentation problem

--- troclaux/packer.lua
+++ troclaux/packer.lua
@@ -42,6 +42,7 @@ return require('packer').startup(function(use)
     use ('tpope/vim-fugitive')
     use ('tpope/vim-surround')
     use ('tpope/vim-commentary')
+    use ('tpope/vim-sleuth')
     use ('ThePrimeagen/vim-be-good')
     use ('ThePrimeagen/harpoon')
 
