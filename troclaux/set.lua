2323f6e686736d5b25466f38f306fe90784ae7af
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-28 21:09:28 -0300

    chore: separate settings in related blocks

--- troclaux/set.lua
+++ troclaux/set.lua
@@ -1,13 +1,18 @@
 vim.o.clipboard = "unnamedplus"
-vim.o.shiftwidth = 4
+
 vim.o.number = true
 vim.o.relativenumber = true
+
 vim.o.wrap = true
-vim.o.autoindent = true
-vim.o.expandtab = true
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
-vim.o.hlsearch = true
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.autoindent = true
 vim.opt.smartindent = true
+
+vim.o.hlsearch = true
 vim.opt.incsearch = true
 
