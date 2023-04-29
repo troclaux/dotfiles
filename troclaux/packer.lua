2ee3b03b047cc2c8956d21d91500c2d9e5739b2f
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-28 21:09:38 -0300

    feat: add lsp plugin

--- troclaux/packer.lua
+++ troclaux/packer.lua
@@ -4,19 +4,40 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.1',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use ('folke/tokyonight.nvim')
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use ('mbbill/undotree')
-  use ('tpope/vim-fugitive')
-  use ('tpope/vim-surround')
+    use ('folke/tokyonight.nvim')
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use ('mbbill/undotree')
+    use ('tpope/vim-fugitive')
+    use ('tpope/vim-surround')
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
 
 end)
