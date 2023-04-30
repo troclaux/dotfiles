b11b5c499284fcb9b7e6d994c4e456ba821be2e1
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-30 15:20:02 -0300

    refactor: add old set to kickstart

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -2,4 +2,11 @@ vim.g.mapleader = " "
 
 vim.opt.relativenumber = true
 
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.opt.scrolloff = 8
+
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
