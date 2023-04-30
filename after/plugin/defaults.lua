59043acb09d411fd9b0dae4d98ce7a0402867d4c
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-04-30 17:15:20 -0300

    chore: remove useless shortcuts

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -22,20 +22,10 @@ vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true })
 
 -- Map <leader>q to close the current buffer.
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true })
-
--- Map <leader>e to open vim file explorer
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.api.nvim_set_keymap('n', '<leader>q', ':q!<CR>', { noremap = true })
 
 -- Map <leader>x to change word and go to next instance
 vim.api.nvim_set_keymap('n', '<Leader>r', '*``cgn', {noremap = true})
-
--- Map <leader>h and <leader>l to go to start and end of line
-vim.api.nvim_set_keymap('n', '<Leader>h', '_', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>l', '$', { noremap = true })
-
--- Map <leader>enter to run bash command inside vim
-vim.api.nvim_set_keymap('n', '<Leader><CR>', ':!', { noremap = true })
 
 -- Map J and K to reposition highlighted text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -44,4 +34,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Center viewport on highlighted search
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- control+d and control+u will keep viewport centralized
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
 
