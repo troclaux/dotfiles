2c8cb57cd31e5f1770d6de38eac11218d2e3466f
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-01 09:03:05 -0300

    feat: add symlink for custom plugins

--- setup_kickstart.sh
+++ setup_kickstart.sh
@@ -1,2 +1,5 @@
-ln -s ~/dotfiles/plugins/surround.lua ~/.config/nvim/lua/custom/plugins/surround.lua
+rm -rdf ~/.config/nvim/lua/custom/plugins && \
+ln -s ~/dotfiles/plugins ~/.config/nvim/lua/custom/plugins && \
+
+rm -rdf ~/.config/nvim/after && \
 ln -s ~/dotfiles/after ~/.config/nvim/after
