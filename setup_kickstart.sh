85e31725c0f7574e0bda3f30671a8b15b6c92314
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-01 10:43:24 -0300

    feat: add shell command to display full file path

--- setup_kickstart.sh
+++ setup_kickstart.sh
@@ -1,5 +1,11 @@
+
+# create symlinks for plugins, settings and remaps
+
 rm -rdf ~/.config/nvim/lua/custom/plugins && \
 ln -s ~/dotfiles/plugins ~/.config/nvim/lua/custom/plugins && \
 
 rm -rdf ~/.config/nvim/after && \
 ln -s ~/dotfiles/after ~/.config/nvim/after
+
+# change init.lua so that lualine displays full file path
+sed -i '/component_separators/s/\(\s*\)\(component_separators\s*=\s*[^,]*,\)/\1\2\n\1path = 2,/' ~/.config/nvim/init.lua
