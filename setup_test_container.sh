a42d50079ce6878807068409c60a6e20f51a1534
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-02 17:24:13 -0300

    feat: add script that sets up docker container

--- setup_test_container.sh
+++ setup_test_container.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-echo -e "Select action:\n(1)Build image only\n(2)Run container only\n(3)Build image and run"
+echo -e "Select action:\n(1)Build image only\n(2)Run container only\n(3)Build image and run container"
 read action
-echo -e "Select Dockerfile distro:\n(1)Ubuntu\n(2)Fedora"
+echo -e "\nSelect Dockerfile distro:\n(1)Ubuntu\n(2)Fedora"
 read distro
 
 if [[ $action -eq 1 || $action -eq 3 ]]; then
+
     if [[ $distro -eq 1 ]]; then
-        docker build -t my-ubuntu-image ./ubuntu/Dockerfile
+        docker build -t my-ubuntu-image ./ubuntu/
     elif [[ $distro -eq 2 ]]; then
-        docker build -t my-fedora-image ./fedora/Dockerfile
+        docker build -t my-fedora-image ./fedora/
     else
-        echo -e "Invalid value inserted for linux distribution, insert 1 or 2"
+        echo -e "Invalid value inserted for linux distribution, insert 1 or 2" 
+        exit 1
     fi
 
-elif [[ $action -eq 2 || $action -eq 3 ]]; then
+fi
+
+if [[ $action -eq 2 || $action -eq 3 ]]; then
+
     if [[ $distro -eq 1 ]]; then
         docker run -it -v ~/dotfiles:/home/dotfiles my-ubuntu-image
     elif [[ $distro -eq 2 ]]; then
@@ -21,6 +26,8 @@ elif [[ $action -eq 2 || $action -eq 3 ]]; then
     else
         echo -e "Invalid value inserted for linux distribution, insert 1 or 2"
     fi
+
 else
     echo -e "Invalid value inserted for action, insert 1, 2 or 3"
+    exit 1
 fi
