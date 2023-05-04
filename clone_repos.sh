a6300804c829a9de4c713ecbd8c59fc9e88f97db
Arthur <arthur.albuquerque.1995@gmail.com>
2023-05-04 17:01:21 -0400

    feat: define cloning folder location

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/adonovan/gopl.io/ && \
-git clone https://github.com/jwasham/coding-interview-university && \
-git clone https://github.com/MichaelCade/90DaysOfDevOps && \
-git clone https://github.com/bregman-arie/devops-exercises
+git clone https://github.com/adonovan/gopl.io/ ~/Documents/gopl.io && \
+git clone https://github.com/jwasham/coding-interview-university ~/Documents/coding-interview-university && \
+git clone https://github.com/MichaelCade/90DaysOfDevOps ~/Documents/90DaysOfDevOps && \
+git clone https://github.com/bregman-arie/devops-exercises ~/Documents/devops-exercises 
