2007fc4d01a13adfa4ff17d00e774301c8d0ffae
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-05 13:24:54 -0300

    chore: remove useless script that is in ansible

--- clone_repos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/adonovan/gopl.io/ ~/Documents/gopl.io && \
-git clone https://github.com/jwasham/coding-interview-university ~/Documents/coding-interview-university && \
-git clone https://github.com/MichaelCade/90DaysOfDevOps ~/Documents/90DaysOfDevOps && \
-git clone https://github.com/bregman-arie/devops-exercises ~/Documents/devops-exercises 
