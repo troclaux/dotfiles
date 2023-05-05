da5fe40d240298d292944532799583112f140899
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-05 20:01:59 -0300

    feat: add script that creates symlinks of scripts

---
+++ setup_scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo ln -s ~/dotfiles/bin/tmux-sessionizer /usr/local/bin/tmux-sessionizer && \
+sudo ln -s ~/dotfiles/bin/tmux-windowizer /usr/local/bin/tmux-windowizer && \
+sudo ln -s ~/dotfiles/bin/tmux-cht.sh /usr/local/bin/tmux-cht.sh && \
+sudo ln -s ~/dotfiles/bin/tmux-tvui /usr/local/bin/tmux-tvui
