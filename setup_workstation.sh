0c65d682b9ad209f7a35e5dfb3cdf7e4ca70452d
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-05 20:12:48 -0300

    feat: make setup faster with dnf confs

--- setup_workstation.sh
+++ setup_workstation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf && \
+grep -q "fastestmirror=True" /etc/dnf/dnf.conf || echo "fastestmirror=True" >> /etc/dnf/dnf.conf && \
+grep -q "deltarpm=True" /etc/dnf/dnf.conf || echo "deltarpm=True" >> /etc/dnf/dnf.conf && \
 sudo dnf upgrade -y && \
 sudo dnf install git -y && \
 git clone https://github.com/troclaux/dotfiles && \
-ansible-playbook local.yml -K --skip-tags "zsh_plugins"
+ansible-playbook local.yml -K --skip-tags "zsh_plugins,clone"
