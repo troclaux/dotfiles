89f802751e5db379310b7f81cfd1ab522314e3b6
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2023-05-06 19:38:09 -0300

    doc: comment with ansible vault

--- setup_workstation.sh
+++ setup_workstation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# ansible-playbook -t dotfiles --ask-vault-pass local.yml --skip-tags "zsh_plugins, clone"
+
 grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf && \
 grep -q "fastestmirror=True" /etc/dnf/dnf.conf || echo "fastestmirror=True" >> /etc/dnf/dnf.conf && \
 grep -q "deltarpm=True" /etc/dnf/dnf.conf || echo "deltarpm=True" >> /etc/dnf/dnf.conf && \
