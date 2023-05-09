3912483e6eda4ddd9837c2e648cdac99af5b6ee2
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-09 20:40:34 -0300

    doc: fix comment spacing

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -56,7 +56,7 @@ vim.api.nvim_set_keymap('n', '<leader>i', ':only<CR>', { noremap = true })
 -- Git push
 vim.api.nvim_set_keymap('n', '<leader>gp', ':Git push<CR>', { noremap = true })
 
---git pull
+-- Git pull
 vim.api.nvim_set_keymap('n', '<leader>gu', ':Git pull<CR>', { noremap = true })
 
 -- Search commit messages ( git -log -S <pattern> )
