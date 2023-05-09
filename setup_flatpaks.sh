5c07b80a9107f252f177c035e7aaaf18e6b325e8
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-09 15:30:24 -0300

    feat: script to install flatpaks

---
+++ setup_flatpaks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+flatpak install -y flathub org.telegram.desktop
+flatpak install -y flathub org.gnome.Extensions
+flatpak install -y flathub com.discordapp.Discord 
+flatpak install -y flathub org.qbittorrent.qBittorrent 
+flatpak install -y flathub com.obsproject.Studio  
+flatpak install -y flathub com.mattjakeman.ExtensionManager 
+flatpak install -y flathub org.videolan.VLC
+flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub com.google.Chrome
