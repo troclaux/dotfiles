62fc26ff9c857b50d0582547ea5df45ba4b48770
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-11 11:59:58 -0300

    feat: add description to telescope remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' } )
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find Grep' } )
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers' } )
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find Help' } )
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find Old Files' } )
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find Git Files' } )
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
