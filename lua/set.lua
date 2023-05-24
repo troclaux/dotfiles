0dea93b7588962e752afb8aa40552687b7f71053
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-24 08:24:49 -0300

    feat: add neovim settings

---
+++ lua/set.lua
@@ -0,0 +1,15 @@
+vim.opt.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+
+vim.opt.scrolloff = 8
+
+vim.o.hlsearch = true
+vim.opt.incsearch = true
