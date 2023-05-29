0941dfbc7ad2ccc82045f1765f8267ef9a583374
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-05-29 15:54:42 -0300

    feat: install parser for python and golang

--- init.lua
+++ init.lua
@@ -115,7 +115,7 @@ require('telescope').setup()
 require('nvim-treesitter.configs').setup({
 
     -- A list of parser names, or "all"
-    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust" },
+    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "go", "python" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
