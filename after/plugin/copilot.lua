5fcc3fa5bbd243601094cc0b88c96e19aa3611e4
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-01 09:09:29 -0300

    chore: remove copilot remaps

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,12 @@
-vim.api.nvim_set_var('copilot_assume_mapped', true)
-vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")', { expr = true, silent = true } )
+-- local suggestion = require('copilot.suggestion')
+
+-- vim.keymap.set('i', '<C-ç>', suggestion.accept_word(), { desc = 'Copilot Accept Word' } )
+
+-- require("copilot.suggestion").is_visible()
+-- require("copilot.suggestion").accept(modifier)
+-- require("copilot.suggestion").accept_word()
+-- require("copilot.suggestion").accept_line()
+-- require("copilot.suggestion").next()
+-- require("copilot.suggestion").prev()
+-- require("copilot.suggestion").dismiss()
+-- require("copilot.suggestion").toggle_auto_trigger()
