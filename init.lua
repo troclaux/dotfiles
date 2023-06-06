9dade07cadf2128d780e4e19e1162203ae11086e
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-06 15:57:16 -0300

    feat: add lsp-zero to neovim

--- init.lua
+++ init.lua
@@ -152,6 +152,15 @@ require('nvim-treesitter.configs').setup({
     },
 })
 
+-- Setup lsp-zero
+local lsp = require('lsp-zero').preset({})
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+end)
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.setup()
+
 
 require("remap")
 require("set")
