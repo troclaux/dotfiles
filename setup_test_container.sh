1183c9ad098702946f3b5330332fcbd9761fd7c9
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-06-16 21:33:48 -0300

    refactor: script for docker containers

--- setup_test_container.sh
+++ setup_test_container.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
-echo -e "Select action:\n(1)Build image only\n(2)Run container only\n(3)Build image and run container"
+echo -e "Select action:\n(1)Build image\n(2)Run container"
 read action
 echo -e "\nSelect Dockerfile distro:\n(1)Ubuntu\n(2)Fedora"
 read distro
 
-if [[ $action -eq 1 || $action -eq 3 ]]; then
+case $action in
+    1 ) 
+        case $distro in
+            1 ) docker build -t my-ubuntu-image ./ubuntu/ ;;
+            2 ) docker build -t my-fedora-image ./fedora/ ;;
+            * ) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
+        esac ;;
+    2 )
+        case $distro in
+            1 ) docker run -it -v ~/dotfiles:/root/dotfiles my-ubuntu-image ;;
+            2 ) docker run -it -v ~/dotfiles:/root/dotfiles my-fedora-image ;;
+            * ) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
+        esac ;;
+    * ) echo -e "Invalid value inserted for action, insert 1 or 2" && exit 1 ;;
+esac
 
-    if [[ $distro -eq 1 ]]; then
-        docker build -t my-ubuntu-image ./ubuntu/
-    elif [[ $distro -eq 2 ]]; then
-        docker build -t my-fedora-image ./fedora/
-    else
-        echo -e "Invalid value inserted for linux distribution, insert 1 or 2" 
-        exit 1
-    fi
-
-fi
-
-if [[ $action -eq 2 || $action -eq 3 ]]; then
-
-    if [[ $distro -eq 1 ]]; then
-        docker run -it -v ~/dotfiles:/root/dotfiles my-ubuntu-image
-    elif [[ $distro -eq 2 ]]; then
-        docker run -it -v ~/dotfiles:/root/dotfiles my-fedora-image
-    else
-        echo -e "Invalid value inserted for linux distribution, insert 1 or 2"
-    fi
-
-else
-    echo -e "Invalid value inserted for action, insert 1, 2 or 3"
-    exit 1
-fi
