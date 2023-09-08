02914c2fbd53463e153762b4b9458d2f8bc59471
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-09-08 14:06:17 -0300

    remove unnecessary config for dnf conf

--- setup_fedora_workstation.sh
+++ setup_fedora_workstation.sh
@@ -3,7 +3,6 @@
 # ansible-playbook -t dotfiles --ask-vault-pass local.yml --skip-tags "zsh_plugins, clone"
 
 grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf && \
-grep -q "fastestmirror=True" /etc/dnf/dnf.conf || echo "fastestmirror=True" >> /etc/dnf/dnf.conf && \
 grep -q "deltarpm=True" /etc/dnf/dnf.conf || echo "deltarpm=True" >> /etc/dnf/dnf.conf && \
 sudo dnf upgrade -y && \
 sudo dnf install git -y && \
