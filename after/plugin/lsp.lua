9c53174030a5936e51f83b79fb06aaeb66ffc2f7
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2023-12-09 10:14:03 -0300

    add linter and typescript server for nvim

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,5 +7,7 @@ end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').tsserver.setup({})
+require('lspconfig').eslint.setup({})
 
 lsp.setup()
