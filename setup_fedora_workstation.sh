8ed6a01562c1558c6b043c929c17243d173e0690
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-13 21:16:53 -0300

    remove deprecated comment

--- setup_fedora_workstation.sh
+++ setup_fedora_workstation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ansible-playbook -t dotfiles --ask-vault-pass local.yml --skip-tags "zsh_plugins, clone"
-
 grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf && \
 grep -q "deltarpm=True" /etc/dnf/dnf.conf || echo "deltarpm=True" >> /etc/dnf/dnf.conf && \
 sudo dnf upgrade -y && \
