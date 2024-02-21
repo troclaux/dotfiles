74d18ee752c68e4fb302417e20efc5c64ba3bda0
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-21 08:05:31 -0300

    add new remaps to nvim fugitive plugin

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,7 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Git stash' } )
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = 'Git Stash' } )
+vim.keymap.set('n', '<Leader>gp', ':Git push<CR>', { noremap = true, desc = 'Git Push' })
+vim.keymap.set('n', '<Leader>gu', ':Git pull<CR>', { noremap = true, desc = 'Git Pull' })
+vim.keymap.set('n', '<Leader>gl', ':GcLog -S ', { noremap = true, desc = 'Search git commit messages' })
+
 vim.keymap.set("n", "gh", "<C>diffget //2<CR>")
 vim.keymap.set("n", "gl", "<C>diffget //3<CR>")
