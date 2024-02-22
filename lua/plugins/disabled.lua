b6e9587acb27f541010812726aa25516662124cb
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-22 19:34:52 -0300

    fix: add nvim plugin that closes pairs (eg parenthesis)

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,7 +4,6 @@ return {
 	{ "rcarriga/nvim-notify", enabled = false },
 	{ "echasnovski/mini.surround", enabled = false },
 	{ "echasnovski/mini.ai", enabled = false },
-	{ "echasnovski/mini.pairs", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "folke/noice.nvim", enabled = false },
