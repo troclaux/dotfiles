64cda40baabe332bfef9eafcf4e90580e19dea10
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-22 20:03:42 -0300

    add nvim git keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -130,3 +130,13 @@ vim.keymap.set(
 
 -- Source current file
 vim.keymap.set("n", "<Leader>so", ":source<CR>", { noremap = true })
+
+-- Plugin keymaps
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status" })
+vim.keymap.set("n", "<Leader>gp", ":Git push<CR>", { noremap = true, desc = "Git Push" })
+vim.keymap.set("n", "<Leader>gu", ":Git pull<CR>", { noremap = true, desc = "Git Pull" })
+vim.keymap.set("n", "<Leader>gl", ":GcLog -S ", { noremap = true, desc = "Search git commit messages" })
+
+vim.keymap.set("n", "gh", "<C>diffget //2<CR>")
+vim.keymap.set("n", "gl", "<C>diffget //3<CR>")
