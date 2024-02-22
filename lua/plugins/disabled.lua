04f4b76cb9c143cc406322c666b47db148b4a0f5
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-22 16:13:45 -0300

    remove lazyvim plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,9 @@
 return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "jesseduffield/lazygit", enabled = false },
+	{ "rcarriga/nvim-notify", enabled = false },
+	{ "echasnovski/mini.surround", enabled = false },
+	{ "folke/persistence.nvim", enabled = false },
+	{ "folke/flash.nvim", enabled = false },
+	{ "RRethy/vim-illuminate", enabled = false },
 }
