850bed2932e9ca300fc84789fdf07f7ea6a3fcfc
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-23 11:19:50 -0300

    add copilot plugin keymaps for nvim

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+-- local suggestion = require('copilot.suggestion')
+
+-- vim.keymap.set('i', '<C-ç>', suggestion.accept_word(), { desc = 'Copilot Accept Word' } )
+
+-- require("copilot.suggestion").is_visible()
+-- require("copilot.suggestion").accept(modifier)
+-- require("copilot.suggestion").accept_word()
+-- require("copilot.suggestion").accept_line()
+-- require("copilot.suggestion").next()
+-- require("copilot.suggestion").prev()
+-- require("copilot.suggestion").dismiss()
+-- require("copilot.suggestion").toggle_auto_trigger()
