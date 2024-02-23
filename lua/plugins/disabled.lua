aa2df69df840a8ea87d9006755212d6b356b69d0
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-23 10:57:49 -0300

    enable notification plugin for nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "jesseduffield/lazygit", enabled = false },
-	{ "rcarriga/nvim-notify", enabled = false },
 	{ "echasnovski/mini.surround", enabled = false },
 	{ "echasnovski/mini.ai", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
