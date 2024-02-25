6d0797feb7057943fdec50cbb3944c9c1cfeb15f
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-02-25 18:09:57 -0300

    add description to nvim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -103,8 +103,8 @@ vim.keymap.set("n", "<Leader>o", ":q<CR>", { noremap = true, desc = "Quit vim" }
 vim.keymap.set("n", "<Leader>w", ":w<CR>", { noremap = true, desc = "Save current buffer" })
 
 -- Add blank line above and below
-vim.keymap.set("n", "[ ", "mzO<Esc>`z", { noremap = true, desc = "Save current buffer" })
-vim.keymap.set("n", "] ", "mzo<Esc>`z", { noremap = true, desc = "Save current buffer" })
+vim.keymap.set("n", "[ ", "mzO<Esc>`z", { noremap = true, desc = "Add blank line above" })
+vim.keymap.set("n", "] ", "mzo<Esc>`z", { noremap = true, desc = "Add blank line below" })
 
 -- Make a script executable
 vim.keymap.set(
