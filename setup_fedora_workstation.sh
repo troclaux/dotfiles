0ee774c9bb8098a760c272c50ce189a53c0a99fa
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-04 18:23:38 -0300

    remove unnecessary code

--- setup_fedora_workstation.sh
+++ setup_fedora_workstation.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >> /etc/dnf/dnf.conf && \
-grep -q "deltarpm=True" /etc/dnf/dnf.conf || echo "deltarpm=True" >> /etc/dnf/dnf.conf && \
-sudo dnf upgrade -y && \
-sudo dnf install git -y && \
-git clone https://github.com/troclaux/dotfiles && \
-# ansible-playbook local.yml -K --skip-tags "clone"
+grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >>/etc/dnf/dnf.conf
+sudo dnf upgrade -y
+sudo dnf install git -y
 ansible-playbook local.yml -K
