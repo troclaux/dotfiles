011aae76dd2870a7412b033819ff173f42820f82
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-04 18:24:05 -0300

    define target folder for git clone command

--- setup_fedora_workstation.sh
+++ setup_fedora_workstation.sh
@@ -3,4 +3,5 @@
 grep -q "max_parallel_downloads=" /etc/dnf/dnf.conf || echo "max_parallel_downloads=5" >>/etc/dnf/dnf.conf
 sudo dnf upgrade -y
 sudo dnf install git -y
+git clone https://github.com/troclaux/dotfiles ~/dotfiles/
 ansible-playbook local.yml -K
