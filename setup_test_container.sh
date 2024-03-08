b1dd9335ae1905137b3fc43a1dd2f4a8083f7874
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-07 21:01:46 -0300

    format code according to linter guidelines

--- setup_test_container.sh
+++ setup_test_container.sh
@@ -5,18 +5,19 @@ echo -e "\nSelect Dockerfile distro:\n(1)Ubuntu\n(2)Fedora"
 read distro
 
 case $action in
-    1 ) 
-        case $distro in
-            1 ) docker build -t my-ubuntu-image ./ubuntu/ ;;
-            2 ) docker build -t my-fedora-image ./fedora/ ;;
-            * ) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
-        esac ;;
-    2 )
-        case $distro in
-            1 ) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-ubuntu-image ;;
-            2 ) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-fedora-image ;;
-            * ) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
-        esac ;;
-    * ) echo -e "Invalid value inserted for action, insert 1 or 2" && exit 1 ;;
+1)
+	case $distro in
+	1) docker build -t my-ubuntu-image ./ubuntu/ ;;
+	2) docker build -t my-fedora-image ./fedora/ ;;
+	*) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
+	esac
+	;;
+2)
+	case $distro in
+	1) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-ubuntu-image ;;
+	2) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-fedora-image ;;
+	*) echo -e "Invalid value inserted for linux distribution, insert 1 or 2" && exit 1 ;;
+	esac
+	;;
+*) echo -e "Invalid value inserted for action, insert 1 or 2" && exit 1 ;;
 esac
-
