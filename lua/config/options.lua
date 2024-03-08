a5358765f5d555f1ed2712aec26784b39dc624f1
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-07 21:02:26 -0300

    show invisible characters on nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,11 @@
 vim.opt.whichwrap = "lh"
 vim.opt.conceallevel = 0
+vim.opt.list = true
+vim.opt.listchars:append({
+	-- eol = '↲',
+	-- tab = "»»",
+	nbsp = "␣",
+	trail = "•",
+	extends = ">",
+	precedes = "<",
+})
