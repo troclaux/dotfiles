0c4b8901e8a5fc250368951c43db90defaacf251
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-10 15:08:57 -0300

    refactor script to get its own list of languages internally

--- bin/tmux-cht.sh
+++ bin/tmux-cht.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+
+languages=$(echo "golang nodejs javascript typescript tmux zsh c lua rust python bash css html sql" | tr " " "\n")
+core_utils1=$(echo "ps grep curl find awk tr mv kill lsof less head tail kubectl" | tr " " "\n")
+core_utils2=$(echo "tar cp rm jq cat ssh cargo chown make xargs stow chmod" | tr " " "\n")
+core_utils2=$(echo "git git-worktree git-status git-commit git-rebase docker docker-compose" | tr " " "\n")
+
+selected=$(echo -e "$languages\n$core_utils1\n$core_utils2\n$core_utils3" | fzf)
+
 if [[ -z $selected ]]; then
-    exit 0
+	exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+if echo "$languages" | grep -qs "$selected"; then
+	query=$(echo $query | tr " " "+")
+	tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
-
