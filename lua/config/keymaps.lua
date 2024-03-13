32dbd595fc13ece63a070a68d459e07b8d5f268b
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-13 15:11:40 -0300

    remove noremap parameter because its default value is true

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,106 +5,96 @@ vim.keymap.del("n", "<Leader>l")
 -- Insert mode remaps --
 
 -- Map jk and kj to go to normal mode
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
-vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
 -- vim.keymap.set("v", "S", "<nop>", { noremap = true })
 
 -- Visual mode remaps --
 
--- Reposition highlighted text in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, desc = "Move selection down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, desc = "Move selection up" })
+-- Move visual selection up or down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- Select until start of line in visual mode
-vim.keymap.set("v", "H", "_", { noremap = true, desc = "Go to start of line" })
+vim.keymap.set("v", "H", "_", { desc = "Go to start of line" })
 
 -- Keep buffer after pasting over selection
-vim.keymap.set("v", "p", "p`[v`]y", { noremap = true, desc = "Go to start of line" })
+vim.keymap.set("v", "p", "p`[v`]y", { desc = "Go to start of line" })
 
 -- Select until end of line in visual mode
-vim.keymap.set("v", "L", "$h", { noremap = true, desc = "Go to end of line" })
+vim.keymap.set("v", "L", "$h", { desc = "Go to end of line" })
 
 -- Align text in visual mode around equals sign
-vim.keymap.set("v", "<Leader>=", ":!column -t<CR>gv=", { noremap = true, desc = "Column Align" })
+vim.keymap.set("v", "<Leader>=", ":!column -t<CR>gv=", { desc = "Column Align" })
 
 -- Select all
-vim.keymap.set("v", "<Leader>a", "<Esc>GVgg", { noremap = true, desc = "Select all" })
+vim.keymap.set("v", "<Leader>a", "<Esc>GVgg", { desc = "Select all" })
 
 -- Search and replace
-vim.keymap.set("v", "<Leader>se", ":s/", { noremap = true, desc = "Search and replace command" })
+vim.keymap.set("v", "<Leader>se", ":s/", { desc = "Search and replace command" })
 
 -- run normal command on visual selection
-vim.keymap.set("v", "<Leader>n", ":normal ", { noremap = true, desc = "Run normal mode command" })
+vim.keymap.set("v", "<Leader>n", ":normal ", { desc = "Run normal mode command" })
 
 -- run macro on visual selection
-vim.keymap.set("v", "<Leader>m", ":normal @q<CR>", { noremap = true, desc = "Run macro on visual selection" })
+vim.keymap.set("v", "<Leader>m", ":normal @q<CR>", { desc = "Run macro on visual selection" })
 
 -- Normal mode remaps --
 
 -- Go to start of line
-vim.keymap.set("n", "H", "_", { noremap = true, desc = "Go to start of line" })
+vim.keymap.set("n", "H", "_", { desc = "Go to start of line" })
 
 -- Go to end of line
-vim.keymap.set("n", "L", "$", { noremap = true, desc = "Go to end of line" })
+vim.keymap.set("n", "L", "$", { desc = "Go to end of line" })
 
 -- Join current line with line below and keep cursor on same position
-vim.keymap.set("n", "J", "mzJ`z", { noremap = true })
+vim.keymap.set("n", "J", "mzJ`z")
 
 -- Reselect pasted text
-vim.keymap.set("n", "gp", "`[v`]", { noremap = true, desc = "Reselect pasted text" })
+vim.keymap.set("n", "gp", "`[v`]", { desc = "Reselect pasted text" })
 
 -- Select all
-vim.keymap.set("n", "<Leader>a", "GVgg", { noremap = true, desc = "Select all text" })
+vim.keymap.set("n", "<Leader>a", "GVgg", { desc = "Select all text" })
 
 -- Keybind for tmux-sessionizer
-vim.keymap.set(
-	"n",
-	"<C-f>",
-	"<cmd>silent !tmux neww tmux-sessionizer<CR>",
-	{ noremap = true, desc = "tmux sessionizer" }
-)
--- vim.keymap.set('n', '<Leader>t', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { noremap = true })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "tmux sessionizer" })
+-- vim.keymap.set('n', '<Leader>t', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 
 -- Center viewport on highlighted search
-vim.keymap.set("n", "n", "nzzzv", { noremap = true })
-vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Go to previous buffer
-vim.keymap.set("n", "<C-b>", "<C-^>", { noremap = true, desc = "Go to previous buffer" })
+vim.keymap.set("n", "<C-b>", "<C-^>", { desc = "Go to previous buffer" })
 
 -- Keep viewport centralized when using ctrl+d and ctrl+u
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 -- Run last terminal command
-vim.keymap.set("n", "<Leader>t", ":@:<CR>", { noremap = true, silent = true, desc = "Run last terminal cmd" })
+vim.keymap.set("n", "<Leader>t", ":@:<CR>", { silent = true, desc = "Run last terminal cmd" })
 
 -- Change word and go to next instance
-vim.keymap.set("n", "<Leader>rr", "*``cgn", { noremap = true, desc = "Replace word and go to next instance" })
+vim.keymap.set("n", "<Leader>r", "*``cgn", { desc = "Replace word and go to next instance" })
 
 -- Search and replace
-vim.keymap.set("n", "<Leader>se", ":%s/", { noremap = true, desc = "Search and replace command" })
+vim.keymap.set("n", "<Leader>se", ":%s/", { desc = "Search and replace command" })
 
 -- Split/Join line of code
--- vim.keymap.set("n", "<Leader>T", ":TSJToggle<CR>", { noremap = true, desc = "Split/Join line of code" })
+-- vim.keymap.set("n", "<Leader>T", ":TSJToggle<CR>", { desc = "Split/Join line of code" })
 
 -- Quit vim
-vim.keymap.set("n", "<Leader>o", ":q<CR>", { noremap = true, desc = "Quit vim" })
+vim.keymap.set("n", "<Leader>o", ":q<CR>", { desc = "Quit vim" })
 
 -- Save current buffer
-vim.keymap.set("n", "<Leader>w", ":w<CR>", { noremap = true, desc = "Save current buffer" })
+vim.keymap.set("n", "<Leader>i", ":w<CR>", { desc = "Save current buffer" })
 
 -- Add blank line above or below
-vim.keymap.set("n", "[ ", "mzO<Esc>0D`z", { noremap = true, desc = "Add blank line above" })
-vim.keymap.set("n", "] ", "mzo<Esc>0D`z", { noremap = true, desc = "Add blank line below" })
+vim.keymap.set("n", "[ ", "mzO<Esc>0D`z", { desc = "Add blank line above" })
+vim.keymap.set("n", "] ", "mzo<Esc>0D`z", { desc = "Add blank line below" })
 
 -- Make a script executable
-vim.keymap.set(
-	"n",
-	"<Leader>xx",
-	":w<bar>!chmod +x %<cr>",
-	{ noremap = true, desc = "Make script executable", silent = true }
-)
+vim.keymap.set("n", "<Leader>xx", ":w<bar>!chmod +x %<cr>", { desc = "Make script executable", silent = true })
 
 -- Open vim file explorer
 vim.keymap.set("n", "<Leader>e", ":Ex<CR>", { noremap = true, desc = "Open vim file explorer" })
