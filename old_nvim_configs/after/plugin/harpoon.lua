1f6aa0c415880e17371b752a010118861eaf0c3c
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-03-15 10:54:55 -0300

    improve readability of plugin's keymaps

--- old_nvim_configs/after/plugin/harpoon.lua
+++ old_nvim_configs/after/plugin/harpoon.lua
@@ -1,14 +1,14 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>m", mark.add_file, { desc = 'Harpoon mark' } )
-vim.keymap.set("n", "<leader>n", ui.toggle_quick_menu, { desc = 'Harpoon menu' } )
-vim.keymap.set("n", "<leader>cc", mark.clear_all, { desc = 'Harpoon clear marks' } )
+vim.keymap.set("n", "<leader>m", mark.add_file, { desc = "Harpoon mark" })
+vim.keymap.set("n", "<leader>n", ui.toggle_quick_menu, { desc = "Harpoon menu" })
+vim.keymap.set("n", "<leader>cc", mark.clear_all, { desc = "Harpoon clear marks" })
 
-vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end, { desc = 'Navigate mark 1' } )
-vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end, { desc = 'Navigate mark 2' } )
-vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end, { desc = 'Navigate mark 3' } )
-vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end, { desc = 'Navigate mark 4' } )
+vim.keymap.set("n", "<leader>h", ui.nav_file(1), { desc = "Navigate mark 1" })
+vim.keymap.set("n", "<leader>j", ui.nav_file(2), { desc = "Navigate mark 2" })
+vim.keymap.set("n", "<leader>k", ui.nav_file(3), { desc = "Navigate mark 3" })
+vim.keymap.set("n", "<leader>l", ui.nav_file(4), { desc = "Navigate mark 4" })
 
--- vim.keymap.set("n", "<leader>.", function() ui.nav_next() end, { desc = 'Next Harpoon mark' } )
--- vim.keymap.set("n", "<leader>,", function() ui.nav_prev() end, { desc = 'Previous Harpoon mark' } )
+-- vim.keymap.set("n", "<leader>.", ui.nav_next(), { desc = "Next Harpoon mark" })
+-- vim.keymap.set("n", "<leader>,", ui.nav_prev(), { desc = "Previous Harpoon mark" })
