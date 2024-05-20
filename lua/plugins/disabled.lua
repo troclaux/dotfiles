7cfe5149ae8eceda9af5f22e29472a3fdc1bffe0
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-05-20 13:36:35 -0300

    style: remove comment

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "jesseduffield/lazygit", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
-	-- { "echasnovski/mini.ai", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "folke/noice.nvim", enabled = false },
