7cb78729eb96a671cb9490e9d9337de268578813
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-05-22 09:14:14 -0300

    feat: script to install all flatpaks

--- install_flatpaks.sh
+++ install_flatpaks.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 flatpak install -y flathub org.telegram.desktop
-flatpak install -y flathub org.gnome.Extensions
-flatpak install -y flathub com.discordapp.Discord 
-flatpak install -y flathub org.qbittorrent.qBittorrent 
-flatpak install -y flathub com.obsproject.Studio  
-flatpak install -y flathub com.mattjakeman.ExtensionManager 
 flatpak install -y flathub org.videolan.VLC
 flatpak install -y flathub com.brave.Browser
 flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub com.bitwarden.desktop
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.jgraph.drawio.desktop
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub md.obsidian.Obsidian
+flatpak install -y flathub com.obsproject.Studio
+flatpak install -y flathub org.kde.okular
+flatpak install -y flathub org.gnome.Extensions
+flatpak install -y flathub org.qbittorrent.qBittorrent
+flatpak install -y flathub com.getpostman.Postman
+flatpak install -y flathub org.gimp.GIMP
