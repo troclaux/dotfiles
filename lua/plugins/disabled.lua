4fbf68094ba58554c662d404a515a9fa5dc4e1c6
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-06-16 07:56:19 -0300

    chore(nvim): remove useless plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "jesseduffield/lazygit", enabled = false },
 	{ "echasnovski/mini.pairs", enabled = false },
+	{ "echasnovski/mini.ai", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "folke/noice.nvim", enabled = false },
