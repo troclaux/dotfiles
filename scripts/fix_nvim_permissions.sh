7b93b60babeec3a59bf876600af24faf383919de
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-07-19 09:11:23 -0300

    fix: script correctly gives permissions for nvim directory

--- scripts/fix_nvim_permissions.sh
+++ scripts/fix_nvim_permissions.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ -d "~/.config/nvim" ]; then
-	sudo chown -R $USER ~/.config/nvim/
-	echo "Successfully changed ownership of ~/.config/nvim/ directory"
-else
-	echo "Failed to change ownership of ~/.config/nvim. Directory does not exist"
-fi
+sudo chown -R $USER ~/.config/nvim/
