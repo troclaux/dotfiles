740293ae3863d111c4e7680d5d8c9a8da9eeb14c
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-02 20:44:18 -0300

    chore(nvim): add plugin requirement

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -10,6 +10,18 @@ return {
 	"preservim/tagbar",
 	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+		keys = {
+			-- suggested keymap
+			-- { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		},
+	},
+	{
 		"Wansmer/treesj",
 		lazy = true,
 		-- keys = { '<space>m', '<space>j', '<space>s' },
