927ae3c85dd61c28d0f206e31f4ea255c36592a3
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-03 17:12:07 -0300

    fix(nvim): avante plugin now works correctly

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,8 +1,7 @@
 return {
 	"yetone/avante.nvim",
-	lazy = true,
 	event = "VeryLazy",
-	build = "make",
+	lazy = false,
 	opts = {
 		mappings = {
 			ask = "<leader>ai",
@@ -38,13 +37,35 @@ return {
 			},
 		},
 	},
+	-- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
+	-- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+	build = ":AvanteBuild source=false",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		--- The below is optional, make sure to setup it properly if you have lazy=true
+		--- The below dependencies are optional,
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+					-- required for Windows users
+					use_absolute_path = true,
+				},
+			},
+		},
 		-- {
+		-- 	-- Make sure to setup it properly if you have lazy=true
 		-- 	"MeanderingProgrammer/render-markdown.nvim",
 		-- 	opts = {
 		-- 		file_types = { "markdown", "Avante" },
