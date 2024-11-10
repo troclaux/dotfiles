cf4078604e2c59bd6b3fba928e4ece6d72bfeaac
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-11-10 10:56:09 -0300

    fix(bin): fuzzy search is now case insensitive

--- bin/finder.sh
+++ bin/finder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected_file=$(find ~/Documents ~/Downloads ~/learning -mindepth 1 -maxdepth 4 | grep -vE "\.git|\.md|\.txt" | wofi --matching=fuzzy --show dmenu)
+selected_file=$(find ~/Documents ~/Downloads ~/learning -mindepth 1 -maxdepth 4 | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu)
 if [ -n "$selected_file" ]; then
 	xdg-open "$selected_file"
 fi
