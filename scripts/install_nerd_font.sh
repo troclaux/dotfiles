ac351c38a82980c41fa0f6d5baf2a542564d0bf0
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-11-11 10:17:37 -0300

    chore: remove unnecessary comment

--- scripts/install_nerd_font.sh
+++ scripts/install_nerd_font.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# if gnome terminal is not rendering the font correctly, try installing another font
-
 latest_url=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep -o 'https://.*JetBrainsMono.zip') &&
 	curl -fLo ~/Downloads/JetBrainsMono.zip "$latest_url" &&
 	unzip -o ~/Downloads/JetBrainsMono.zip -d ~/Downloads/JetBrainsMono &&
