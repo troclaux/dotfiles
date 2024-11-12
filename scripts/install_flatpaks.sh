79b4575a8e158982be6d952d45ee63fe178d28d5
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-11-12 15:22:21 -0300

    feat: add new flatpak

--- scripts/install_flatpaks.sh
+++ scripts/install_flatpaks.sh
@@ -9,10 +9,14 @@ flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.jgraph.drawio.desktop
 flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub com.github.tchx84.Flatseal
-flatpak install -y flathub md.obsidian.Obsidian
 flatpak install -y flathub com.obsproject.Studio
 flatpak install -y flathub org.kde.okular
 flatpak install -y flathub org.gnome.Extensions
 flatpak install -y flathub org.qbittorrent.qBittorrent
 flatpak install -y flathub com.getpostman.Postman
 flatpak install -y flathub org.gimp.GIMP
+flatpak install -y flathub it.mijorus.gearlever
+
+# flatpak install -y flathub md.obsidian.Obsidian
+# flatpak install -y flathub org.audacityteam.Audacity
+# flatpak install -y flathub io.github.nokse22.asciidraw
