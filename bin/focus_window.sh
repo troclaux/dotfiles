dad5344b2725b8e4277d726e0f7bfcc5456b30e2
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-11-17 08:11:51 -0300

    chore(bin): add prompt to script

--- bin/focus_window.sh
+++ bin/focus_window.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected_window=$(swaymsg -t get_tree | grep -E '"app_id":|"class":' | grep -v 'null' | awk -F': ' '{print $2}' | tr -d '", ' | sort -u | wofi -i --matching=fuzzy --show dmenu)
+selected_window=$(swaymsg -t get_tree | grep -E '"app_id":|"class":' | grep -v 'null' | awk -F': ' '{print $2}' | tr -d '", ' | sort -u | wofi -i --matching=fuzzy --show dmenu -p "Select window to focus")
 
 if [ -n "$selected_window" ]; then
   swaymsg [app_id="$selected_window"] focus
