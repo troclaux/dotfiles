cfbd39451b2a41f13d72aa4a6ba3a2b0d2970a19
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-07 06:41:16 -0300

    add prompt to script

--- bin/finder.sh
+++ bin/finder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected_file=$(find ~/Documents ~/Downloads -mindepth 1 -maxdepth 4 | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu)
+selected_file=$(find ~/Documents ~/Downloads ~/Videos -mindepth 1 -maxdepth 4 | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
 
 if [ -n "$selected_file" ]; then
 	xdg-open "$selected_file"
