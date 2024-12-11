84ba9c28a0f5dcff20a1b8f28ae4dd3cfc4d2a1a
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-11 19:11:08 -0300

    print selected action

--- setup_test_container.sh
+++ setup_test_container.sh
@@ -11,11 +11,29 @@ echo -e "Select action:\n\
 read action
 
 case $action in
-1) docker build -t my-ubuntu-image ./ubuntu/ ;;
-2) docker build -t my-fedora-image ./fedora/ ;;
-3) docker run -it --rm -v ~/dotfiles:/root/dotfiles --entrypoint /bin/bash my-ubuntu-image ;;
-4) docker run -it --rm -v ~/dotfiles:/root/dotfiles --entrypoint /bin/bash my-fedora-image ;;
-5) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-ubuntu-image ;;
-6) docker run -it --rm -v ~/dotfiles:/root/dotfiles my-fedora-image ;;
+1)
+  echo "Building Ubuntu image..."
+  docker build -t my-ubuntu-image ./ubuntu/
+  ;;
+2)
+  echo "Building Fedora image..."
+  docker build -t my-fedora-image ./fedora/
+  ;;
+3)
+  echo "Starting Ubuntu container with bash..."
+  docker run -it --rm -v ~/dotfiles:/root/dotfiles --entrypoint /bin/bash my-ubuntu-image
+  ;;
+4)
+  echo "Starting Fedora container with bash..."
+  docker run -it --rm -v ~/dotfiles:/root/dotfiles --entrypoint /bin/bash my-fedora-image
+  ;;
+5)
+  echo "Starting Ubuntu container with ansible playbook..."
+  docker run -it --rm -v ~/dotfiles:/root/dotfiles my-ubuntu-image
+  ;;
+6)
+  echo "Starting Fedora container with ansible playbook..."
+  docker run -it --rm -v ~/dotfiles:/root/dotfiles my-fedora-image
+  ;;
 *) echo -e "Invalid value inserted for action, insert a number between 1 and 6" && exit 1 ;;
 esac
