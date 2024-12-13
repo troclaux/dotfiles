1864b0bdeb809687a8e4eef66874a6baae0f089b
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-13 20:53:10 -0300

    chore(nvim): disable lazyvim's animations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 vim.opt.whichwrap = "lh"
 vim.opt.conceallevel = 0
 vim.opt.scrolloff = 8
+vim.g.snacks_animate = false
 vim.opt.formatoptions:remove({ "c", "r", "o" })
 vim.opt.list = true
 vim.opt.listchars:append({
