a5e6a2e8c69636622dd2a053d27593391761074a
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-18 11:09:41 -0300

    feat(nvim): add oil plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		keymaps = {
+			["<C-p>"] = false,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
