881724a51ce5cc84705ca26e42f7cbaa1dc9b7ce
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-20 11:50:49 -0300

    chore(nvim): update plugin configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,40 +1,43 @@
 return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
-	build = ":Copilot auth",
-	opts = {
-		-- suggestion = { enabled = false },
-		suggestion = {
-			enabled = true,
-			auto_trigger = true,
-			debounce = 75,
-			keymap = {
-				accept_word = "<C-j>",
-				accept_line = "<C-l>",
-				accept = false,
-				next = "<C-v>",
-				prev = "<C-b>",
-				dismiss = "<C-]>",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			opts = {
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept_word = "<C-j>",
+						accept_line = "<C-l>",
+						accept = false,
+						next = "<C-v>",
+						prev = "<C-b>",
+						dismiss = "<C-]>",
+					},
+				},
+				panel = {
+					enabled = true,
+					auto_refresh = true,
+					keymap = {
+						jump_prev = "[g",
+						jump_next = "]g",
+						accept = "<CR>",
+						refresh = "gr",
+						open = "<C-q>",
+					},
+					layout = {
+						position = "right", -- bottom | top | left | right
+						ratio = 0.4,
+					},
+				},
+				filetypes = {
+					markdown = true,
+					["*"] = true,
+				},
 			},
-		},
-		panel = {
-			enabled = true,
-			auto_refresh = true,
-			keymap = {
-				jump_prev = "[g",
-				jump_next = "]g",
-				accept = "<CR>",
-				refresh = "gr",
-				open = "<C-q>",
-			},
-			layout = {
-				position = "right", -- bottom | top | left | right
-				ratio = 0.4,
-			},
-		},
-		filetypes = {
-			markdown = true,
-			["*"] = true,
-		},
-	},
+		})
+	end,
 }
