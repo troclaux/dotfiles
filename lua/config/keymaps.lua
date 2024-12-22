259c552b2b82ff0e40e4dd8de0b0ca33d600409f
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-22 09:47:00 -0300

    fix: keymap error when yanking entire line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -109,6 +109,12 @@ vim.keymap.set("n", "<Leader>pf", "o- [ ] ", { desc = "Insert markdown form" })
 vim.keymap.set("n", "<Leader>pc", "p`[v`]omzo<Esc>o<Esc>S```<Esc>`zO```", { desc = "Paste and surround with ```" })
 vim.keymap.set("v", "<Leader>pc", "omzo<Esc>o```<Esc>`zO```", { desc = "Surround with ```" })
 vim.keymap.set("n", "<Leader>pi", "pmz`[v`]o<Esc>i`<Esc>`zla`<Esc>", { desc = "Paste and surround with `" })
+vim.keymap.set("n", "<leader>pI", function()
+	local text = vim.fn.getreg('"')
+	text = text:gsub("^%s+", ""):gsub("%s+$", "")
+	local surrounded = "`" .. text .. "`"
+	vim.api.nvim_put({ surrounded }, "c", false, true)
+end, { desc = "Paste text and surround with backticks" })
 
 vim.keymap.set("n", "<Leader>1", 'o<Esc>0"_Di# ', { desc = "Insert markdown heading #" })
 vim.keymap.set("n", "<Leader>2", 'o<Esc>0"_Di## ', { desc = "Insert markdown heading ##" })
