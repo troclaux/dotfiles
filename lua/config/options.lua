4fdbc12eaeb17ce9a5a3ba6bd16682005907aa81
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-29 09:23:36 -0300

    feat: add nvim option for nvim's avante plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
+vim.g.snacks_animate = false
+vim.opt.laststatus = 3
 vim.opt.whichwrap = "lh"
 vim.opt.conceallevel = 0
 vim.opt.scrolloff = 8
-vim.g.snacks_animate = false
 vim.opt.formatoptions:remove({ "c", "r", "o" })
 vim.opt.list = true
 vim.opt.listchars:append({
