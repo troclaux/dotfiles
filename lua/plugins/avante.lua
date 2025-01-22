c3a6ec8dcb8227668f1186b8839c196910442e1b
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-01-22 08:15:43 -0300

    chore(avante): change keymaps

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,7 +4,7 @@ return {
 	lazy = true,
 	opts = {
 		mappings = {
-			ask = "<leader>ai",
+			-- ask = "<leader>ai",
 			-- edit = "<leader>ae",
 			-- refresh = "<leader>ar",
 			--- @class AvanteConflictMappings
@@ -34,7 +34,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		--- The below dependencies are optional,
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"echasnovski/mini.icons", -- or nvim-tree/nvim-web-devicons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
 		{
 			-- support for image pasting
