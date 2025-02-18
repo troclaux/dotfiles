7ce81a9de64c3c0d26427ba64db1f549b8111b9c
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-18 15:41:45 -0300

    feat: add pdfs in documents folder to search

--- bin/finder.sh
+++ bin/finder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-selected_file=$(find ~/books ~/Documents ~/Downloads ~/Videos -mindepth 1 -maxdepth 4 | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
+combined_output=$( (
+	ls ~/Documents/*/pdfs/*.pdf
+	find -L ~/books ~/Documents ~/Downloads ~/Videos -mindepth 1 -maxdepth 4
+))
+
+selected_file=$(echo "$combined_output" | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
 
 if [ -n "$selected_file" ]; then
 	xdg-open "$selected_file"
