b893dcacaf1c4e9efbe7ce2f65fa07adb677097d
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-02-25 10:59:42 -0300

    fix: finder now lists files in Downloads and Videos directory

--- bin/finder.sh
+++ bin/finder.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-combined_output=$( (
-	ls ~/Documents/*/pdfs/*.pdf
-	find -L ~/books ~/Documents ~/Downloads ~/Videos -mindepth 1 -maxdepth 4
-))
+pdf_files=$(ls ~/Documents/*/pdfs/*.pdf 2>/dev/null || echo "")
+
+find_files=$(find -L ~/Videos ~/Downloads ~/books ~/Documents -mindepth 1 -maxdepth 4 -type f 2>/dev/null)
+
+combined_output=$(printf "%s\n%s" "$pdf_files" "$find_files")
 
 selected_file=$(echo "$combined_output" | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
 
