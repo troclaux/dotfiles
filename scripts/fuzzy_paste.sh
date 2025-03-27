e3d2a9457f9069ee512b8f8cd6d0139bdebf65dc
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-03-27 10:28:39 -0300

    feat: paste selection

--- scripts/fuzzy_paste.sh
+++ scripts/fuzzy_paste.sh
@@ -3,12 +3,12 @@
 file="$HOME/dotfiles/scripts/buffer.txt"
 
 if [ ! -f "$file" ]; then
-	echo "No file found in the current directory."
-	exit 1
+	nvim "$file"
 fi
 
 selected_line=$(cat "$file" | wofi -i --matching=fuzzy --show dmenu -p "Select text to copy")
 
 if [ -n "$selected_line" ]; then
 	echo -n "$selected_line" | wl-copy
+	wtype "$selected_line"
 fi
