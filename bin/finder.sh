b643487f3861e2a454ca80a6be5606d821b44e90
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-04-16 21:03:56 -0300

    fix: remove duplicates in results

--- bin/finder.sh
+++ bin/finder.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-pdf_files=$(find ~/Documents/*/pdfs -maxdepth 1 -type f -name "*.pdf" 2>/dev/null)
-
 find_files=$(find -L ~/Videos ~/Downloads ~/books ~/Documents -mindepth 1 -maxdepth 3 -type f 2>/dev/null)
 
-combined_output=$(printf "%s\n%s" "$pdf_files" "$find_files")
-
-selected_file=$(echo "$combined_output" | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
+selected_file=$(echo "$find_files" | grep -vE "\.git|\.md|\.txt" | wofi -i --matching=fuzzy --show dmenu -p "Select file to open")
 
 if [ -n "$selected_file" ]; then
 	xdg-open "$selected_file"
