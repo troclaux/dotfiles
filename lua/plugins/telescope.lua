90d905b06435b2588e780544c5db4191443b4ac4
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-04-21 17:18:57 -0300

    feat(nvim): add new telescope plugin keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,8 @@ return {
 	keys = {
 		{ "<C-p>", "<cmd>Telescope git_files<CR>", desc = "Find Git Files" },
 		{ "<C-h>", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+		{ "<C-j>", "<cmd>Telescope live_grep<CR>", desc = "Find Files" },
+		{ "<C-k>", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Workspace Symbols" },
 		-- { "<Leader>sp", require("telescope.builtin").git_commits, desc = "Find Vim Options" },
 	},
 }
