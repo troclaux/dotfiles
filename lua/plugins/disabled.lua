9335570aa1cd9ba84c476cc3c3a86292686bacc0
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2026-05-15 16:34:15 -0300

    chore: remove disabled plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,11 +1,21 @@
 return {
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "jesseduffield/lazygit", enabled = false },
-	{ "echasnovski/mini.pairs", enabled = false },
+	{ "nvim-mini/mini.pairs", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "folke/noice.nvim", enabled = false },
-	{ "ggandor/leap.nvim", enabled = false },
-	{ "ggandor/flit.nvim", enabled = false },
+	{ url = "https://codeberg.org/andyg/leap.nvim.git", enabled = false },
+	{ url = "https://codeberg.org/andyg/flit.nvim.git", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
+	-- Disable lazygit keymaps from snacks.nvim (using vim-fugitive instead)
+	{
+		"folke/snacks.nvim",
+		keys = {
+			{ "<leader>gg", false },
+			{ "<leader>gG", false },
+			{ "<leader>gf", false },
+			{ "<leader>gl", false },
+			{ "<leader>gL", false },
+		},
+	},
 }
